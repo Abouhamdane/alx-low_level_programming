3f5ed1c2097f43ab86b2d0ff9bd70c9dcaa2485f
abouhamdane <abouhamdane.med@gmail.com>
2023-06-15 16:36:00 -0700

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,6 @@
+#include«stdio.h>
+* main - Entry point
+* Return: Always 0 (Success)
+int main(void)
+printf/'with proper grammar, but the qutcome is a piece of art in");
+return (0)
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include stdio h»
+* main - Entry point
+* Return: Always 0 (Success)
+int main(void)
+print."Size of a char: %d byte(sjin*, sizeof(char)) print 'Size of an int. %d byte(syn" szeofint)y printf/'Size of a long int. %6d bytels let sizoof(long int)y. printf/"Size of a long long int %6d byte(sjr?, sizeof(long lorig int))
+printi"Size of = toar
+t)
+
